3722f576c50fa9dd928004fca40c93f8ac357dfb
crgoga <crgoga+ncstate@ncsu.edu>
2026-07-28 10:21:02 -0400

    updating string_rotation with textbook solution
    more interesting solution that what I thought of

--- 1. Array and Hash Table/9. String Rotation/string_rotation.cpp
+++ 1. Array and Hash Table/9. String Rotation/string_rotation.cpp
@@ -9,6 +9,8 @@ class StringRotation {
             return false; // can't be rotation without same length
         }
 
+        /** my solution
+         * 
         bool same = false; // indicator of same sequence of characters between the two strings
         int end = 0; // index of where to end the string to check substring
         int p1 = 0; // pointer for s1
@@ -42,8 +44,22 @@ class StringRotation {
         // cout << s2.substr(0, end) << endl;
         return isSubstring(s1, s2.substr(0, end));
 
+        */
 
+        // textbook solution that is better and more thought provoking tbh
+
+        /** if s2 is a rotation will be a substring of the string s1s1 because of the breakpoint 
+         * found within s2. imagine s1 as xy and s2 as yx where yx is the opposite of the breakpoint found
+         * within s1, if we double s1 to get xyxy, we can see that yx in right in the middle.
+        */
+
+        if (s1.length() != 0) {
+            string s1s1 = s1 + s1;
+            return isSubstring(s1s1, s2);
+        }
+        return false;
     }
+
     private:
     bool isSubstring(string s1, string s2) {
         return (s1.find(s2) != string::npos || s2.find(s1) != string::npos);
